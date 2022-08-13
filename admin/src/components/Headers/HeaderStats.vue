73125720166676909deb28e981612a89b5cf9808
Bui Van Trung <84084472+trungmark@users.noreply.github.com>
2022-08-13 18:07:56 +0700

    cm1

---
+++ admin/src/components/Headers/HeaderStats.vue
@@ -0,0 +1,54 @@
+<template>
+  <!-- Header -->
+  <div class="relative bg-emerald-600 md:pt-32 pb-32 pt-12">
+    <div class="px-4 md:px-10 mx-auto w-full">
+      <div>
+        <!-- Card stats -->
+        <div class="flex flex-wrap">
+          <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
+            <card-stats
+              statSubtitle="Total User"
+              statTitle="350,897"
+              statIconName="fas fa-users"
+              statIconColor="bg-red-500"
+            />
+          </div>
+          <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
+            <card-stats
+              statSubtitle="Total Doctor"
+              statTitle="350,897"
+              statIconName="fas fa-user-md"
+              statIconColor="bg-orange-500"
+            />
+          </div>
+          <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
+            <card-stats
+              statSubtitle="Total Specialist"
+              statTitle="49,65%"
+              statIconName="fas fa-users"
+              statIconColor="bg-red-400"
+            />
+          </div>
+          <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
+            <card-stats
+              statSubtitle="Medicine Type"
+              statTitle="49,65%"
+              statIconName="fas fa-capsules"
+              statIconColor="bg-emerald-500"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CardStats from "@/components/Cards/CardStats.vue";
+
+export default {
+  components: {
+    CardStats,
+  },
+};
+</script>
---
+++ admin/src/services/MedicineService.js
@@ -0,0 +1,29 @@
+import http from "@/http-common";
+
+/* eslint-disable */
+class MedicineService {
+  getAll() {
+    return http.get("/api/medicines");
+  }
+
+  getById(id) {
+    return http.get(`/api/medicines/${id}`);
+  }
+
+  create(data) {
+    return http.post("/api/medicines", data);
+  }
+
+  update(id, data) {
+    return http.put(`/api/medicines/${id}`, data);
+  }
+
+  delete(id) {
+    return http.delete(`/api/medicines/${id}`);
+  }
+  uploadMainImage(file){
+    return http.post("/api/uploadFile");
+  }
+
+}
+export default new MedicineService();
